39fc3ca64e0e73f4668fd651ac58af63a0081950
joeybail96 <joey.bail@utah.edu>
2025-06-02 13:10:15 -0600

    reorganizing code to execute certain plots based on user prompt responses

--- plot.py
+++ plot.py
@@ -4,7 +4,6 @@ import os
 
 processor = TifProcessor()
 
-
 base_path = "/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/in_process_data"
 coards_nc = os.path.join(base_path, "1US_EC_coards.nc")
 mask_nc = os.path.join(base_path, "2US_EC_mask.nc")
@@ -14,92 +13,80 @@ reprojected_nc = os.path.join(base_path, "4bUS_EC_reprojected.nc")
 regridded_nc = os.path.join(base_path, "5bUS_EC_regridded.nc")
 chloride_nc = os.path.join(base_path, "6bUS_EC_cl_factored.nc")
 
-#bounding_box = (-113, -112.5, 39.5, 40)
-#bounding_box = (-114, -112, 38, 40)
 bounding_box = (-125, -100, 30, 45)
-#bounding_box = (-130, -64, 21, 53)
 bounding_box_str = f"bb_{bounding_box[0]:.2f}_{bounding_box[1]:.2f}_{bounding_box[2]:.2f}_{bounding_box[3]:.2f}"
 
-# plot raw data
-nc2plot = coards_nc
-ds = xr.open_dataset(nc2plot)
-ds_trimmed = processor.trim_to_latlon_box(ds, bounding_box)
-var2plot = 'EC'
-colormap = 'viridis'
-nc_base_name = os.path.basename(nc2plot).replace(".nc", "")
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_{nc_base_name}_{var2plot}.png"
-#processor.plot_ds_trimmed(ds_trimmed, var2plot, colormap, save_path = output_png_path, show_colorbar=False, show_values=False, colorbar_limits=(0,20))
+def prompt_and_plot(prompt_text, plot_func):
+    if input(f"{prompt_text} (y/n): ").strip().lower() == "y":
+        plot_func()
 
+# --- Raw data plot ---
+def plot_raw():
+    ds = xr.open_dataset(coards_nc)
+    ds_trimmed = processor.trim_to_latlon_box(ds, bounding_box)
+    output_png_path = f"../figures/{bounding_box_str}_1US_EC_coards_EC.png"
+    processor.plot_ds_trimmed(ds_trimmed, 'EC', 'viridis', save_path=output_png_path, show_colorbar=False, show_values=False, colorbar_limits=(0, 20))
 
-# plot fine data
-fine2plot = mask_nc
-ds1 = xr.open_dataset(fine2plot)
-ds1_trimmed = processor.trim_to_latlon_box(ds1, bounding_box)
-var2plot = 'Playa_Mask'
-colormap = 'Greys'
-nc_base_name = os.path.basename(fine2plot).replace(".nc", "")
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_{nc_base_name}_{var2plot}.png"
-processor.plot_ds_trimmed(ds1_trimmed, var2plot, colormap, save_path = output_png_path, show_colorbar=False, show_values=False)
+prompt_and_plot("Plot raw data?", plot_raw)
 
+# --- Fine mask plot ---
+def plot_fine():
+    ds1 = xr.open_dataset(mask_nc)
+    ds1_trimmed = processor.trim_to_latlon_box(ds1, bounding_box)
+    output_png_path = f"../figures/{bounding_box_str}_2US_EC_mask_Playa_Mask.png"
+    processor.plot_ds_trimmed(ds1_trimmed, 'Playa_Mask', 'Greys', save_path=output_png_path, show_colorbar=False, show_values=False)
 
-# plot coarse data
-coarse2plot = coarsened_nc
-ds2 = xr.open_dataset(coarse2plot)
-ds2_trimmed = processor.trim_to_latlon_box(ds2, bounding_box)
-var2plot = 'Playa_Mask'
-colormap = 'Greys'
-nc_base_name = os.path.basename(coarse2plot).replace(".nc", "")
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_{nc_base_name}_{var2plot}.png"
-processor.plot_ds_trimmed(ds2_trimmed, var2plot, colormap, save_path = output_png_path, show_colorbar=False, show_values=False)
+prompt_and_plot("Plot fine resolution mask?", plot_fine)
 
+# --- Coarse mask plot ---
+def plot_coarse():
+    ds2 = xr.open_dataset(coarsened_nc)
+    ds2_trimmed = processor.trim_to_latlon_box(ds2, bounding_box)
+    output_png_path = f"../figures/{bounding_box_str}_3bUS_EC_coarsened_Playa_Mask.png"
+    processor.plot_ds_trimmed(ds2_trimmed, 'Playa_Mask', 'Greys', save_path=output_png_path, show_colorbar=False, show_values=False)
 
-# plot fine and coarse
-nc_base_name = "fine_coarse"
-var2plot = 'Playa_Mask'
-colormap = 'Greys'
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_3b{nc_base_name}_{var2plot}.png"
-#processor.plot_ds_together(ds1_trimmed, ds2_trimmed, var2plot, colormap, save_path = output_png_path, show_colorbar=False)
+prompt_and_plot("Plot coarse resolution mask?", plot_coarse)
 
+# --- Fine vs. coarse comparison plot ---
+def plot_fine_coarse():
+    ds1 = xr.open_dataset(mask_nc)
+    ds1_trimmed = processor.trim_to_latlon_box(ds1, bounding_box)
+    ds2 = xr.open_dataset(coarsened_nc)
+    ds2_trimmed = processor.trim_to_latlon_box(ds2, bounding_box)
+    output_png_path = f"../figures/{bounding_box_str}_3b_fine_coarse_Playa_Mask.png"
+    processor.plot_ds_together(ds1_trimmed, ds2_trimmed, 'Playa_Mask', 'Greys', save_path=output_png_path, show_colorbar=False)
 
+prompt_and_plot("Compare fine vs. coarse masks?", plot_fine_coarse)
 
+# --- Reprojected plot ---
+def plot_reprojected():
+    ds1 = xr.open_dataset(reprojected_nc)
+    output_png_path = f"../figures/{bounding_box_str}_4bUS_EC_reprojected_Playa_Mask.png"
+    processor.plot_ds_epsg4326(ds1, 'Playa_Mask', 'Greys', bounding_box, save_path=output_png_path, show_colorbar=False, show_values=False)
 
+prompt_and_plot("Plot reprojected data?", plot_reprojected)
 
-nc2plot = reprojected_nc
-ds1 = xr.open_dataset(nc2plot)
-colormap = 'Greys'
-nc_base_name = os.path.basename(nc2plot).replace(".nc","")
-var2plot = 'Playa_Mask'
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_{nc_base_name}_{var2plot}.png"
-#processor.plot_ds_epsg4326(ds1, var2plot, colormap, bounding_box, save_path = output_png_path, show_colorbar=False, show_values=False)
+# --- Regridded plot ---
+def plot_regridded():
+    ds2 = xr.open_dataset(regridded_nc)
+    output_png_path = f"../figures/{bounding_box_str}_5bUS_EC_regridded_Playa_Mask.png"
+    processor.plot_ds_epsg4326(ds2, 'Playa_Mask', 'Greys', bounding_box, grid_thickness=0, save_path=output_png_path, show_colorbar=False, show_values=False)
 
+prompt_and_plot("Plot regridded data?", plot_regridded)
 
+# --- Reprojected vs. regridded comparison plot ---
+def plot_reprojected_regridded():
+    ds1 = xr.open_dataset(reprojected_nc)
+    ds2 = xr.open_dataset(regridded_nc)
+    output_png_path = f"../figures/{bounding_box_str}_5b_reprojected_regridded_Playa_Mask.png"
+    processor.plot_ds_together_4326(ds1, ds2, 'Playa_Mask', 'Greys', save_path=output_png_path, show_colorbar=False, bounding_box=bounding_box)
 
+prompt_and_plot("Compare reprojected vs. regridded?", plot_reprojected_regridded)
 
-nc2plot = regridded_nc
-ds2 = xr.open_dataset(nc2plot)
-colormap = 'Greys'
-nc_base_name = os.path.basename(nc2plot).replace(".nc","")
-var2plot = 'Playa_Mask'
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_{nc_base_name}_{var2plot}.png"
-#processor.plot_ds_epsg4326(ds2, var2plot, colormap, bounding_box, grid_thickness=0, save_path = output_png_path, show_colorbar=False, show_values=False)
+# --- Chloride factored plot ---
+def plot_chloride():
+    ds = xr.open_dataset(chloride_nc)
+    output_png_path = f"../figures/{bounding_box_str}_5b_chloride_Playa_Cl.png"
+    processor.plot_ds_epsg4326(ds, 'Playa_Cl', 'viridis', bounding_box, grid_thickness=0, save_path=output_png_path, show_colorbar=False, show_values=False)
 
-
-
-
-# plot fine and coarse
-nc_base_name = "reprojected_regridded"
-var2plot = 'Playa_Mask'
-colormap = 'Greys'
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_5b{nc_base_name}_{var2plot}.png"
-#processor.plot_ds_together_4326(ds1, ds2, var2plot, colormap, save_path = output_png_path, show_colorbar=False, bounding_box=bounding_box)
-
-
-
-
-nc2plot = chloride_nc
-colormap = 'viridis'
-ds = xr.open_dataset(nc2plot)
-var2plot = 'Playa_Cl'
-output_png_path = f"/uufs/chpc.utah.edu/common/home/haskins-group1/users/jbail/GEOSChem/SSURGO/Scripts/figures/{bounding_box_str}_5b{nc_base_name}_{var2plot}.png"
-
-processor.plot_ds_epsg4326(ds, var2plot, colormap, bounding_box, grid_thickness=0, save_path = output_png_path, show_colorbar=False, show_values=False)
+prompt_and_plot("Plot chloride-factored Playa Mask?", plot_chloride)
